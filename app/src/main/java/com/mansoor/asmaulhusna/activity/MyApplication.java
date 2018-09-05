676a30d1eca53754e2f3cd56ef8e6f1cbb0582de
Md Mansoor Kooripparambil <mdmansoor.k@maybank.com>
2018-09-05 16:34:15 +0800

    home screen added

--- app/src/main/java/com/mansoor/asmaulhusna/activity/MyApplication.java
+++ app/src/main/java/com/mansoor/asmaulhusna/activity/MyApplication.java
@@ -3,10 +3,20 @@ package com.mansoor.asmaulhusna.activity;
 import android.app.Application;
 import android.content.res.Configuration;
 
+import com.mansoor.asmaulhusna.models.Prayers;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 /**
  * Created by l4208412 on 18/6/2018.
@@ -19,7 +29,7 @@ public class MyApplication extends Application {
     public String QuotesUrl = "https://raw.githubusercontent.com/kpamansoor/api/master/asmaulhusna/quotes.json";
     public String DailyVerseUrl = "http://api.alquran.cloud/ayah/";
     public String PrayerTimesUrl = "http://api.aladhan.com/v1/calendar?";
-
+    public List<String> prayersList = new ArrayList<>(Arrays.asList("Fajr","Sunrise","Dhuhr","Asr","Sunset","Maghrib","Isha"));
     public String getPrayerTimesUrl() {
         return PrayerTimesUrl;
     }
@@ -29,7 +39,7 @@ public class MyApplication extends Application {
     public String getQuotesUrl() {
         return QuotesUrl;
     }
-
+    public String app_name = "Islamic Hub";
     public String getStringFromInputStream(InputStream is) {
 
         BufferedReader br = null;
@@ -59,6 +69,68 @@ public class MyApplication extends Application {
 
     }
 
+    public  String convert24To12(String time){
+        try {
+            final SimpleDateFormat sdf = new SimpleDateFormat("H:mm");
+            final Date dateObj = sdf.parse(time);
+            return new SimpleDateFormat("K:mm aa").format(dateObj);
+        } catch (final ParseException e) {
+            e.printStackTrace();
+        }
+        return time;
+    }
+    public String dataToText(String text) {
+//        String text = "2015-01-17";
+        try {
+            SimpleDateFormat sdf1 = new SimpleDateFormat("dd-MM-yyyy");
+            SimpleDateFormat sdf2 = new SimpleDateFormat("dd MMM yyyy");
+
+            Date date = sdf1.parse(text);
+
+            return sdf2.format(date);
+        } catch (ParseException exp) {
+            exp.printStackTrace();
+        }
+
+        return text;
+    }
+
+    public Date addDays(Date date, int days) {
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, days);
+
+        return cal.getTime();
+    }
+
+    public String showCurrentPrayer(Date date, Prayers prayers) {
+        int hour = date.getHours();
+        int minut = date.getMinutes();
+        double currentTime = Double.parseDouble(hour+"."+minut);
+        double min, diff ;
+        String nextPrayerName = "",nextPrayerTime = "";
+        Double[] prayersTimes = new Double[7];
+        prayersTimes[0] = Double.parseDouble(prayers.getFajr().split(" ")[0].replace(":","."));
+        prayersTimes[1] = Double.parseDouble(prayers.getSunrise().split(" ")[0].replace(":","."));
+        prayersTimes[2] = Double.parseDouble(prayers.getDhuhr().split(" ")[0].replace(":","."));
+        prayersTimes[3] = Double.parseDouble(prayers.getAsr().split(" ")[0].replace(":","."));
+        prayersTimes[4] = Double.parseDouble(prayers.getSunset().split(" ")[0].replace(":","."));
+        prayersTimes[5] = Double.parseDouble(prayers.getMaghrib().split(" ")[0].replace(":","."));
+        prayersTimes[6] = Double.parseDouble(prayers.getIsha().split(" ")[0].replace(":","."));
+
+        min = prayersTimes[0] - currentTime;
+        for (int i = 0; i < prayersTimes.length; i++) {
+            diff = prayersTimes[i] - currentTime;
+            if(min < 0)
+                min = diff;
+            if(diff >= 0 && diff <= min) {
+                min = diff;
+                nextPrayerTime = convert24To12(String.valueOf(prayersTimes[i]).replace(".",":"));
+                nextPrayerName = prayersList.get(i);
+            }
+        }
+        return nextPrayerName+","+nextPrayerTime;
+    }
     @Override
     public void onCreate() {
         super.onCreate();
--- app/src/main/java/com/mansoor/asmaulhusna/fragments/HomeFragment.java
+++ app/src/main/java/com/mansoor/asmaulhusna/fragments/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.mansoor.asmaulhusna.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,13 +11,20 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mansoor.asmaulhusna.R;
+import com.mansoor.asmaulhusna.activity.MyApplication;
+import com.mansoor.asmaulhusna.models.Prayers;
+import com.mansoor.asmaulhusna.utils.DBHelper;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,10 +43,13 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private TextView tvTime, tvDate, tvViewAll;
-
+    private SharedPreferences prefs;
+    private TextView tvTime, tvDate, tvViewAll,tvPrayerName,tvPrayerTime,tvLocation;
+    private MyApplication myapp;
+    private ImageView ivShare;
     private OnFragmentInteractionListener mListener;
-
+    private DBHelper mydb;
+    Prayers prayers;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -77,6 +89,15 @@ public class HomeFragment extends Fragment {
         tvTime = view.findViewById(R.id.tvTime);
         tvDate = view.findViewById(R.id.tvDate);
         tvViewAll = view.findViewById(R.id.tvViewAll);
+        tvPrayerName = view.findViewById(R.id.tvPrayerName);
+        tvPrayerTime = view.findViewById(R.id.tvPrayerTime);
+        tvLocation = view.findViewById(R.id.tvLocation);
+        ivShare = view.findViewById(R.id.ivShare);
+        tvViewAll.setText("\uD83D\uDD50 VIEW ALL");
+        prefs = getActivity().getSharedPreferences("asmaulhusna", MODE_PRIVATE);
+        tvLocation.setText(prefs.getString("timezone",""));
+        myapp = ((MyApplication) getActivity().getApplicationContext());
+        getActivity().setTitle("App name");
         final Handler someHandler = new Handler(getContext().getMainLooper());
         someHandler.postDelayed(new Runnable() {
             @Override
@@ -95,7 +116,54 @@ public class HomeFragment extends Fragment {
                 transaction.commit();
             }
         });
+        tvPrayerName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_fragment, ConfigureParyerTimeFragment.newInstance("param1","param2"));
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        ivShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = myapp.app_name+": Prayer timing\nFajr : "+prayers.getFajr()+
+                        "\nSunrise : "+prayers.getSunrise().split(" ")[0]+
+                        "\nDhuhr : "+prayers.getDhuhr().split(" ")[0]+
+                        "\nAsr : "+prayers.getAsr().split(" ")[0]+
+                        "\nSunset : "+prayers.getSunset().split(" ")[0]+
+                        "\nMaghrib : "+prayers.getMaghrib().split(" ")[0]+
+                        "\nIsha : "+prayers.getIsha().split(" ")[0];
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Ayath of the day.");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
+
+        checkForPrayerTimeDB();
         return view;
+    }
+
+    private void checkForPrayerTimeDB() {
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = new Date();
+        tvDate.setText(myapp.dataToText(dateFormat.format(date)));
+        mydb = new DBHelper(getActivity());
+        prayers = mydb.getPrayerTimes(dateFormat.format(date));
+        if(prayers != null){
+            tvViewAll.setVisibility(View.VISIBLE);
+            ivShare.setVisibility(View.VISIBLE);
+            String currentTime = myapp.showCurrentPrayer(date, prayers);
+            tvPrayerName.setText("Next : "+"\uD83D\uDD4C"+currentTime.split(",")[0]);
+            tvPrayerTime.setText(currentTime.split(",")[1]);
+        }else{
+            tvPrayerName.setText("\uD83D\uDD4C Configure Prayer times.");
+            tvPrayerTime.setVisibility(View.VISIBLE);
+
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
