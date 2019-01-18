c2021dd846cea6029fc74ebc52b3343ac10dea7d
Md Mansoor Kooripparambil <mdmansoor.k@maybank.com>
2019-01-18 15:40:11 +0800

    changes

--- app/src/main/java/com/mansoor/asmaulhusna/activity/SplashScreenActivity.java
+++ app/src/main/java/com/mansoor/asmaulhusna/activity/SplashScreenActivity.java
@@ -41,7 +41,7 @@ public class SplashScreenActivity extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        startActivity(new Intent(SplashScreenActivity.this,MainActivity.class),
+                        startActivity(new Intent(SplashScreenActivity.this,LocationActivity.class),
                                 ActivityOptions.makeSceneTransitionAnimation(SplashScreenActivity.this).toBundle());
                         finish();
                     }
